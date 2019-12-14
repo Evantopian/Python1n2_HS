606f5171c766a6948a8f3f71505ac70a88052847
Evan Huang <Evanhuang47@gmail.com>
2019-12-14 04:18:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+Hours = float(input("How many hours did you work?"))
+Pay = float(input("What is your hourly pay?"))
+Total = float(Hours*Pay)
+print (("You get paid $")+ str(Total) + (" in ")+str(Hours)+ (" hours."))
